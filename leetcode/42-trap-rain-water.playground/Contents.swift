fc9ed8709eb4c997111a19d84d4844db2fa7351d
chenxi <991001506@qq.com>
2021-04-07 23:19:05 +0800

    trap rain water method 2

--- leetcode/42-trap-rain-water.playground/Contents.swift
+++ leetcode/42-trap-rain-water.playground/Contents.swift
@@ -29,7 +29,29 @@ class Solution {
         }
         return ans
     }
+    
+    func trap1(_ height: [Int]) -> Int {
+        if height.count < 2 {
+            return 0
+        }
+        var leftMax = Array(repeating: 0, count: height.count)
+        leftMax[0] = height[0]
+        for i in 1..<height.count {
+            leftMax[i] = max(leftMax[i - 1], height[i])
+        }
+        var rightMax = Array(repeating: 0, count: height.count)
+        rightMax[height.count - 1] = height[height.count - 1]
+        for i in stride(from: height.count - 2, through: 0, by: -1) {
+            rightMax[i] = max(height[i], rightMax[i + 1])
+        }
+        var ans = 0
+        for i in 1..<height.count - 1 {
+            ans += min(leftMax[i], rightMax[i]) - height[i]
+        }
+        return ans
+    }
 }
 
 let height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 print(Solution().trap(height))
+print(Solution().trap1(height))
--- leetcode/42-trap-rain-water.py
+++ leetcode/42-trap-rain-water.py
@@ -28,7 +28,25 @@ class Solution(object):
             ans += (min(leftMax, rightMax) - height[i])
         return ans
 
+    def trap1(self, height):
+        if len(height) < 1:
+            return 0
+        size = len(height)
+        leftMax = [0 for _ in range(size)]
+        leftMax[0] = height[0]
+        for i in range(1, size):
+            leftMax[i] = max(height[i], leftMax[i - 1])
+        rightMax = [0 for _ in range(size)]
+        rightMax[size - 1] = height[size - 1]
+        for i in range(size - 2, -1, -1):
+            rightMax[i] = max(height[i], rightMax[i + 1])
+        ans = 0
+        for i in range(1, size - 1):
+            ans += (min(leftMax[i], rightMax[i]) - height[i])
+        return ans
+
 
 height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 # height = [4, 2, 0, 3, 2, 5]
 print Solution().trap(height)
+print Solution().trap1(height)
