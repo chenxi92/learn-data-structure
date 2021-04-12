8d39ad52df21d2755f9045e2d3df3d5ca8f06ece
chenxi <991001506@qq.com>
2021-04-13 00:43:38 +0800

    update combination

--- leetcode/77-combination.py
+++ leetcode/77-combination.py
@@ -10,20 +10,38 @@
 # 1 <= k <= n
 
 class Solution(object):
-    def combine(self, n, k):
+
+    def combine1(self, n, k):
         ans = []
 
-        def dfs(path, start, end, k):
-            if k == 0:
+        def traceback(path, begin):
+            if len(path) == k:
                 ans.append(path[:])
                 return
-            for i in range(start, end + 1):
+            for i in range(begin, n + 1):
                 path.append(i)
-                dfs(path, i + 1, end, k - 1)
+                traceback(path, i + 1)
                 path.pop()
 
-        dfs([], 1, n, k)
+        traceback([], 1)
+        return ans
+
+    def combine2(self, n, k):
+        ans = []
+
+        def traceback(path, begin):
+            if len(path) == k:
+                ans.append(path[:])
+                return
+            # 优化取值范围 [begin, n - (k - len(path)) + 1]
+            for i in range(begin, n - (k - len(path)) + 2):
+                path.append(i)
+                traceback(path, i + 1)
+                path.pop()
+
+        traceback([], 1)
         return ans
 
 
-print Solution().combine(4, 2)
+print Solution().combine1(4, 2)
+print Solution().combine2(4, 2)
