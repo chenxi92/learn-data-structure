2edb934900bb78da39c978cc00f3926364cf339b
chenxi <991001506@qq.com>
2021-05-14 22:34:33 +0800

    素数个数统计-埃式筛选 优化

--- tu-ling/4-素数个数统计-埃式筛选/index.md
+++ tu-ling/4-素数个数统计-埃式筛选/index.md
@@ -30,7 +30,7 @@
          if isPrime[i] is False:
              count += 1
              # j 是合数的标记位
-             j = 2 * i
+             j = i * i
              while j < n:
                  isPrime[j] = True
                  j += i
--- tu-ling/4-素数个数统计-埃式筛选/solution.py
+++ tu-ling/4-素数个数统计-埃式筛选/solution.py
@@ -9,7 +9,7 @@ def eratosthenes(n):
         if isPrime[i] is False:
             count += 1
             # j 是合数的标记位
-            j = 2 * i
+            j = i * i
             while j < n:
                 isPrime[j] = True
                 j += i
